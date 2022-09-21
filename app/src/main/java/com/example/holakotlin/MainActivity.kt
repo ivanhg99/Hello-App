60cd3b5a447620fb31ff28d169a624123fb1c10d
Ivan <ivanhg1999@gmail.com>
2022-09-21 15:22:35 +0200

    Primer mensaje

---
+++ app/src/main/java/com/example/holakotlin/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.holakotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    var tts: TextToSpeech? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tts = TextToSpeech(this, this)
+
+        findViewById<Button>(R.id.btnPlay).setOnClickListener {
+            speak()
+        }
+
+    }
+
+    fun speak(){
+        var message: String = findViewById<EditText>(R.id.eMensaje).text.toString()
+
+        if (message.isEmpty()){
+            message = "Espaliba"
+        }
+
+        findViewById<TextView>(R.id.textView).text = message
+
+        tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS){
+            findViewById<TextView>(R.id.textView).text = "Listo!"
+            tts!!.setLanguage(Locale("ES"))
+        }else {
+            findViewById<TextView>(R.id.textView).text = "No Disponible"
+        }
+    }
+
+    override fun onDestroy() {
+        if (tts != null){
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+
+        super.onDestroy()
+    }
+}
